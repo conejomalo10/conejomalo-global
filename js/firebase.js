56da672e972c71edf49048c18d8b804c33aee7a2
conejomalo10 <official.deconejomalo@gmail.com>
2026-03-07 18:17:31 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,24 @@
 import { initializeApp } from
-"https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
 import { getAuth } from
-"https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+  "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
 
-apiKey: "AIzaSyDpFAL7wX__ee1bmbr25A81IBuWEnpq970",
-authDomain: "conejomalo-global.firebaseapp.com",
-projectId: "conejomalo-global",
-storageBucket: "conejomalo-global.appspot.com",
+  apiKey: "AIzaSyDpFAL7wX__ee1bmbr25A81IBuWEnpq970",
+
+  authDomain: "conejomalo-global.firebaseapp.com",
+
+  projectId: "conejomalo-global",
+
+  storageBucket: "conejomalo-global.firebasestorage.app",
+
 messagingSenderId: "886224291739",
-appId: "1:886224291739:web:f54c336ac7ea4e3512c33e"
+
+  appId: "1:886224291739:web:f54c336ac7ea4e3512c33e",
+
+  measurementId: "G-F07TTHFS7K"
 
 };
 
