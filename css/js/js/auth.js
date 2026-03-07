0f33cb4624fe5ab12b2f52ad79a0af27f6d400fa
conejomalo10 <official.deconejomalo@gmail.com>
2026-03-07 01:33:55 -0300

    Create auth.js

---
+++ css/js/js/auth.js
@@ -0,0 +1,51 @@
+import { auth } from "./firebase.js";
+
+import {
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+onAuthStateChanged,
+signOut
+}
+from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+
+window.register = async function(){
+
+const email=document.getElementById("email").value;
+const password=document.getElementById("password").value;
+
+try{
+await createUserWithEmailAndPassword(auth,email,password);
+alert("Registered Successfully");
+}catch(e){
+alert(e.message);
+}
+
+}
+
+window.login = async function(){
+
+const email=document.getElementById("email").value;
+const password=document.getElementById("password").value;
+
+try{
+await signInWithEmailAndPassword(auth,email,password);
+alert("Login Successful");
+}catch(e){
+alert(e.message);
+}
+
+}
+
+window.logout=function(){
+signOut(auth)
+}
+
+onAuthStateChanged(auth,(user)=>{
+
+if(user){
+document.getElementById("status").innerText="Logged in as "+user.email
+}else{
+document.getElementById("status").innerText="Not logged in"
+}
+
+})
