8f12b30e382811d1e08d4fc4244ec366223ff97a
conejomalo10 <official.deconejomalo@gmail.com>
2026-03-08 21:54:32 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
+import { initializeApp } from
+  "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from
+  "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+const firebaseConfig = {
+  apiKey:            "AIzaSyDpFAL7wX__ee1bmbr25A81IBuWEnpq970",
+  authDomain:        "conejomalo-global.firebaseapp.com",
+  projectId:         "conejomalo-global",
+  storageBucket:     "conejomalo-global.firebasestorage.app",
+  messagingSenderId: "886224291739",
+  appId:             "1:886224291739:web:f54c336ac7ea4e3512c33e",
+  measurementId:     "G-F07TTHFS7K"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
